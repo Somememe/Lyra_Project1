1c8ee8d084e3f254d940ae41b70178e70a431be2
MJOdorczuk <michaljanodorczuk@gmail.com>
2024-02-23 13:51:11 +0100

    Small cleanup for LoRa SD IMU demo

--- u-lora/circuitPython/demos/LoRa_SD_IMU_demo.py
+++ u-lora/circuitPython/demos/LoRa_SD_IMU_demo.py
@@ -7,15 +7,6 @@ from lib import adafruit_mpu6050
 import digitalio
 from lib import adafruit_rfm9x
 
-# Use the board's primary SPI bus
-# Or, use an SPI bus on specific pins:
-#spi = busio.SPI(board.SD_SCK, MOSI=board.SD_MOSI, MISO=board.SD_MISO)
-
-# For breakout boards, you can choose any GPIO pin that's convenient:
-
-# Boards with built in SPI SD card slots will generally have a
-# pin called SD_CS:
-#cs = board.SD_CS
 
 sdcard = sdcardio.SDCard(busio.SPI(board.GP18, MOSI=board.GP19, MISO=board.GP16), board.GP17)
 vfs = storage.VfsFat(sdcard)
@@ -40,11 +31,8 @@ with open("/sd/data.csv", "w") as f:
         #message = "\tAcceleration: X:%.2f, Y: %.2f, Z: %.2f m/s^2\n"%(mpu.acceleration) +\
         #    "\tGyro X:%.2f, Y: %.2f, Z: %.2f degrees/s\n"%(mpu.gyro) +\
         #    "\tTemperature: %.2f C"%mpu.temperature
-        data = [id]
-        data.extend([acc for acc in mpu.acceleration])
-        data.extend([gyr for gyr in mpu.gyro])
-        data.append(mpu.temperature)
-        message = "%d,"%id + "%.2f,%.2f,%.2f,"%(mpu.acceleration) + "%.2f,%.2f,%.2f,"%(mpu.gyro) + "%.2f\n"%mpu.temperature
+        message = "%d,"%id + "%.2f,%.2f,%.2f,"%(mpu.acceleration) \
+            + "%.2f,%.2f,%.2f,"%(mpu.gyro) + "%.2f\n"%mpu.temperature
         f.write(message)
         rfm9x.send(message)
         id += 1
